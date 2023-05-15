8d94e7c918fc57abc25b927c01865f305519810d
ThabangLeeuw <thabangleeuw97@gmail.com>
2023-05-15 15:03:32 +0200

    project commit

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """inheritated class User from BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """the init constructor"""
+        super().__init__(**kwargs)
